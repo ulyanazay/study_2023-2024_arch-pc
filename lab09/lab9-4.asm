178dc8c40e8946aa04efb0af50ca9e4562dd98fa
ulyana <146383985+ulyanazay@users.noreply.github.com>
2023-12-06 00:20:55 +0300

    Add files via upload

---
+++ lab09/lab9-4.asm
@@ -0,0 +1,41 @@
+; 10(x-1)
+%include 'in_out.asm'
+
+SECTION .data
+
+msg db "Результат: ",0
+
+SECTION .text
+
+global _start
+
+_start:
+
+pop ecx
+
+pop edx
+
+sub ecx,1
+
+mov esi, 0
+
+mov edi,10
+call .next
+
+.next:
+pop eax
+call atoi
+add eax,-1
+mul edi
+add esi,eax
+cmp ecx,0h
+jz .done
+loop .next
+
+.done:
+mov eax, msg
+call sprint
+mov eax, esi
+call iprintLF
+call quit
+ret
